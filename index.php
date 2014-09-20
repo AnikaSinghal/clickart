f77956a089283eb828928ff64ab397d1fe6ce8e0
Chirag Jain <chirgjn@users.noreply.github.com>
2014-09-20 18:06:43 +0530

    Added comments & prepared the basic layout for the home page

--- index.php
+++ index.php
@@ -13,17 +13,20 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand text-hide" href="index.html">Clickart</a>
-			</div>
+	<header>
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<section class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand text-hide" href="index.php">Clickart</a>
+				</section>
+				<!-- .navbar-header -->
+
 				<form class="navbar-form navbar-left" role="search">
 					<div class="form-group">
 						<label for="navbar-searchbox" class="sr-only">Search</label>
@@ -35,21 +38,83 @@
 						</div>
 					</div>
 				</form>
-			<div class="navbar-collapse collapse">
-				<form class="navbar-form navbar-right" role="login">
-					<div class="hidden-sm form-group">
-						<label for="email" class="sr-only">Email</label>
-						<input type="text" id="email" placeholder="Email" class="form-control">
+
+				<!-- search form -->
+				<section class="navbar-collapse collapse">
+					<form class="navbar-form navbar-right" role="login">
+						<div class="hidden-sm form-group">
+							<label for="email" class="sr-only">Email</label>
+							<input type="text" id="email" placeholder="Email" class="form-control">
+						</div>
+						<div class="hidden-sm form-group">
+							<label for="password" class="sr-only">Password</label>
+							<input type="password" id="password" placeholder="Password" class="form-control">
+						</div>
+						<button type="submit" class="btn btn-success">Sign in</button>
+					</form>
+					<!-- login form -->
+				</section>
+				<!--.navbar-collapse -->
+			</div>
+			<!-- .container -->
+		</nav>
+		<!-- .navbar -->
+	</header>
+
+	<div class="container body" style="background-color: red;">
+		<div class="row">
+			<div class="main col-md-9" style="min-height: 80vmin; background-color: black;">
+				<div id="carousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel" data-slide-to="1"></li>
+						<li data-target="#carousel" data-slide-to="2"></li>
+					</ol>
+
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="images/blue.png" alt="...">
+							<div class="carousel-caption">
+								.1.
+							</div>
+						</div>
+						<div class="item">
+							<img src="images/red.png" alt="...">
+							<div class="carousel-caption">
+								.2.
+							</div>
+						</div>
+						<div class="item">
+							<img src="images/green.png" alt="...">
+							<div class="carousel-caption">
+								.3.
+							</div>
+						</div>
 					</div>
-					<div class="hidden-sm form-group">
-						<label for="password" class="sr-only">Password</label>
-						<input type="password" id="password" placeholder="Password" class="form-control">
-					</div>
-					<button type="submit" class="btn btn-success">Sign in</button>
-				</form>
-			</div><!--/.navbar-collapse -->
+
+					<!-- Controls -->
+					<!-- <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+					</a>
+					<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</a> -->
+				</div>
+				<!-- .carousel -->
+			</div>
+			<!-- .main -->
+			<aside class="sidebar col-md-3">
+				<div style="min-height: 80vmin; background-color: green;">
+				</div>
+
+			</aside>
+			<!-- .sidebar -->
 		</div>
-	</nav>
+		<!-- .row -->
+	</div>
+	<!-- .body -->
 
 	<!-- jQuery & Bootstrap.js -->
 	<script src="_/js/jquery-2.1.1.js"></script>
